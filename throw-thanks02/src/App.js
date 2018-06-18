cd164d9b0fbd117fb13a24c945f3402aee827b29
takao-h <hayashi_takao@cyberbuzz.co.jp>
2018-06-18 23:11:42 +0900

    Add new AppBar

--- throw-thanks02/src/App.js
+++ throw-thanks02/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppBar from './AppBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
+        </header> */}
+        <AppBar/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
