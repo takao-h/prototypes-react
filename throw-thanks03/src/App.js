f9d989b019a8ab46fd53110278b1e9bfa60d9f1c
takao-h <hayashi_takao@cyberbuzz.co.jp>
2018-06-24 16:29:33 +0900

    add react router

--- throw-thanks03/src/App.js
+++ throw-thanks03/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Cards from './Cards';
+// import { Link } from 'react-router-dom'
+import Button from '@material-ui/core/Button';
+
+
+const link = 'hogehoge';
+const thanksButton = (link) => {
+  <Button component={link} to="/open-collective">
+    Link 
+  </Button>
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Cards />
       </div>
     );
   }
